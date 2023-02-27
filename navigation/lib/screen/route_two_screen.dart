98a783c8276d24290bb5b7d2ed577f088afa9efc
Jungma <mbjun3630@naver.com>
2023-02-27 20:52:32 +0900

    feat: route push replace, remove until

--- navigation/lib/screen/route_two_screen.dart
+++ navigation/lib/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({super.key});
@@ -31,6 +32,49 @@ class RouteTwoScreen extends StatelessWidget {
             );
           },
           child: const Text('Push Named'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // Push Replacement
+            // ㄴ RouteThreeScreen 에서 pop 시 RouteOneScreen으로 이동된다.
+            // 원래라면 [HomeScreen(), RouteOneScreen(), RouteTwoScreen(), RouteThreeScreen()] 스택으로 쌓여야 하지만,
+            // pushReplacement() 는 [HomeScreen(), RouteOneScreen(), RouteThreeScreen()] 으로 기존 스택을 대체 시킨다.
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (_) => const RouteThreeScreen(),
+              ),
+            );
+          },
+          child: const Text('Push Replacement'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed('/three');
+          },
+          child: const Text('Push Replacement Named'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // push 되는 라우트를 제외하고 스택이 지워진다.
+            // 단, 두 번째 파라미터로 기존 라우트 스택에서 비교하여 특정 라우트를 유지 시킬 수 있다.
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                builder: (_) => const RouteThreeScreen(),
+              ),
+              // HomeScreen() 이 유지된다.
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: const Text('Push And Remove Until'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              '/three',
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: const Text('Push Named And Remove Until'),
         ),
       ],
     );
